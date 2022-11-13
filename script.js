96368eab6a1371fed55a2536c148faabe5112496
LordAdhaar <jainadhaar2002@gmail.com>
2022-11-13 15:11:07 +0530

    update gaps and fonts

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("hello world")
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+
+    console.log("hello world")
+    var i;
+    var x = document.getElementsByClassName("trending card");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "flex";
+}
